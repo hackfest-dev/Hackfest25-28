303c6df794d4ba2f4ff579eb774d50dd9d1adc68
Keerthan <kkeerthan2005@gmail.com>
2025-04-19 11:32:03 +0530

    email provider

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
 
 import { db } from "~/server/db";
 
@@ -32,7 +33,17 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
-    Google,
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD
+        }
+      },
+      from: process.env.EMAIL_FROM
+    }),
     /**
      * ...add more providers here.
      *
